fixed normalization by edge type

mp01.py
@@ -7,7 +7,7 @@
 # Normalize weights per edge type.
 # ----------------------------------------------------
 
-import mpfuncs.py as mp
+import mpfuncs as mp
 import numpy as np
 
 
@@ -20,6 +20,7 @@ path = '../networks/'
 
 infile = ename + '.edge.txt'
 outfile = ename + '.edge_norm.txt'
+outdict = ename + '.dict.txt'
 #outfile = ename + '.gene-only.txt'
 delim = '\t'
 
@@ -36,7 +37,7 @@ delim = '\t'
 # BEGIN MAIN FUNCTION
 
 print "Reading in the non-normalized edge file..."
-edgeArray, nodeDict =  mp.readEdgeFile(path+infile, delim)
+edgeArray, nodeDict = mp.readEdgeFile(path+infile, delim)
 
 # get the unique edge types in the network
 eTypes = list( np.unique(edgeArray[:,3]) )
@@ -45,24 +46,35 @@ eTypes.sort()
 print "{} contains the following edges:".format(infile)
 print eTypes
 
+print "Normalizing edge type ..."
 for et in eTypes :
+    print "    {}".format(et)
 
     # not sure this line works
     # may need to make temp array
-    values = edgeArray[edgeArray[:,3]==et, 3]
+    values = edgeArray[edgeArray[:,3]==et, 2]
+    # convert entries from str to float
+    v2 = np.zeros((len(values),1))
+    for i in range(0,len(values)) :
+        v2[i] = float(values[i])
+    #end loop
 
-    vmin = np.amin(values)
-    vmax = np.amin(values)
+#    vmin = np.amin(values)
+#    vmax = np.amin(values)
+    vmin = float(np.amin(v2))
+    vmax = float(np.amax(v2))
     vdif = vmax - vmin
 
-    for i in len(edgeArray)[0] :
+#    print vmin, vmax, vdif
+
+    for i in range(0, edgeArray.shape[0]) :
         if edgeArray[i,3] == et :
 
             if (vdif == 0) :
                 # if vmin=vmax, then there's only one value
                 temp = 1
             else :
-                temp = float((edgeArray[i,2] - vmin) / vdif)
+                temp = (float(edgeArray[i,2]) - vmin) / vdif
             #end if
 
             edgeArray[i,2] = str(temp)
@@ -72,7 +84,7 @@ for et in eTypes :
 #end loop
 
 print "Writing the normalized edge file to {}".format(outfile)
-mp.writeEdgeFile(edgeArray, path+outfile, delim)
+mp.writeEdgeFilePlus(edgeArray, nodeDict, path+outfile, path+outdict, delim)
 
 
 

mpfuncs.py
@@ -25,13 +25,16 @@ def readEdgeFile(datafile, delimiter) :
     nLines = sum( 1 for line in open(datafile, "rb") )
 
     # assign space for edge list
-#    dt = np.dtype('a23')
-    dt = np.dtype.str
+    dt = np.dtype('a30')
+#    dt = np.dtype(str)
+#    dt = np.dtype(object)
+
 #    Edges = np.empty( [nLines,4], dtype=dt)
     Edges = np.empty( (nLines,4), dtype=dt)
 
     # dictionary to hold Node indices
     Nodes = dict()
+    nodeSet = set()
 
     # Start reading from the file
     df = open(datafile, "rb")
@@ -46,17 +49,21 @@ def readEdgeFile(datafile, delimiter) :
         Edges[i] = lv
 
         # add node locations to dict
-        if (lv[0] in Nodes.keys()) :
+#        if (lv[0] in Nodes.keys()) :
+        if (lv[0] in nodeSet) :
             Nodes[lv[0]].append(i)
         else :
             Nodes[lv[0]] = list()
             Nodes[lv[0]].append(i)
+            nodeSet.add(lv[0])
         #end if
-        if (lv[1] in Nodes.keys()) :
+#        if (lv[1] in Nodes.keys()) :
+        if (lv[1] in nodeSet) :
             Nodes[lv[1]].append(i)
         else :
             Nodes[lv[1]] = list()
             Nodes[lv[1]].append(i)
+            nodeSet.add(lv[1])
         #end if
 
         i += 1
@@ -193,7 +200,7 @@ def writeEdgeFilePlus(network, nodeDict, edgefile, dictfile, delimiter) :
 
         # Print each (key, value), where value is a list
         # first print the key
-        g.write("{}{}".format(key, delim))
+        g.write("{}{}".format(key, delimiter))
 
         # then print the list of indeces
         run = 0