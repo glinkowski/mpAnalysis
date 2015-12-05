ec2be10941d4436dabaa5a9b879a932063c79a9a
glinkowski <linkowski@hotmail.com>
2015-12-05 15:36:37 -0600

    script to test the new DFS Walk functions
    Looks at a network, pulls N random genes and counts metapaths
    by calling the old DFS Walk method through the new functions
    stored in mpfuncs.

---
+++ mpDFSWalk01.py
@@ -0,0 +1,85 @@
+# ----------------------------------------------------
+# author: Greg Linkowski
+# project: metapaths for KnowEnG
+#
+# DFS search for metapaths between nodes
+#
+# This is to test the functions built from the original
+#   mpDFSWalk script.
+# From a sample file, select N source genes & N target
+#   genes. Set a maxPathLength. For each source gene,
+#   find all paths of maxPathLength or less. Take note
+#   of what edge types are traversed, and how many
+#   of that MetaPath exist.
+# ----------------------------------------------------
+
+import mpfuncs as mp
+import numpy as np
+import random
+
+
+
+
+######## ######## ######## ########
+# PARAMETERS
+
+maxPathLength = 3
+numSource = numTarget = 1
+
+outpath = "../output/"
+outfile = "mpDFS-test02.txt"
+
+edgename = "toy_hsa_c"
+edgefile = "../networks/" + edgename + ".edge.txt"
+genefile = "../networks/" + edgename + ".genes.txt"
+delim = "\t"
+
+#samppath = "../samples/"
+#sampname = "ACEVEDO_FGFR1_TARGETS_IN_PROSTATE_CANCER_MODEL"
+sampname = "Random_from_all_genes_in_network"
+
+######## ######## ######## ########
+
+
+
+
+######## ######## ######## ########
+# BEGIN MAIN FUNCTION
+
+print ""
+
+# Get the Edges & Nodes from the network file
+print "Reading in the graph ..."
+Edges, Nodes = mp.readEdgeFile(edgefile, delim)
+del Nodes # not needed
+
+
+
+# Get the Genes known to be in the network
+geneSet = set()
+gf = open(genefile, "rb")
+for line in gf:
+    lv = line.split(delim)
+    geneSet.add(lv[0])
+#end loop
+
+# Select N random genes from gene set
+sourceList = list()
+geneList = list(geneSet)
+for n in range(0,numSource) :
+    sourceList.append(random.choice(geneList))
+#end loop
+targetList = sourceList
+del geneList # no longer needed
+
+
+
+# Run the DFS metapath search & save output
+print "Saving metapaths to file: {}".format(outfile)
+
+mp.outputMetaPathsDFS(outpath+outfile, maxPathLength, sampname, 
+    edgename, Edges, sourceList, targetList)
+
+
+
+print "\nDone.\n"
