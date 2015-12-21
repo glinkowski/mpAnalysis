a1933e8df3c512011a7dd999cd4bcf794b98c7fc
glinkowski <linkowski@hotmail.com>
2015-12-21 12:26:12 -0600

    created method to read keep file
    Created and tested a function to read the keep file.
    Returns lists of regex expressions for the genes to
    keep, and the names of the edges to keep. Also,
    returns a list of the indirect edges to be modified
    into direct.

--- preProcFuncs.py
+++ preProcFuncs.py
@@ -1,3 +1,81 @@
 
 
+import os.path
+import sys
+
+
+
+
+######## ######## ######## ########
+# Function: Read in the keep file corresponding to
+#	the specified network edge file
+#	The keep file specifies which genes & edges to
+#	keep in the final product, and provides the
+#	necessary regex characters to identify them.
+# Input:
+#	fname, str - path & name to keep file
+# Returns:
+#	keepGenes - list of Regex expressions for the
+#		genes to keep specified in file
+#	keepEdges - list of Regex expressions for the
+#		edges to keep specified in file
+#	indirEdges - list of indirect edge types
+def readKeepFile(fname) :
+
+	# Make sure the keep file exists
+	if not os.path.isfile(fname) :
+		print "Please create a keep file: ", fname
+		sys.exit()
+	#end if
+
+	# The lists to return
+	keepGenes = list()
+	keepEdges = list()
+	indirEdges = list()
+
+	# Read the file
+	f = open(fname, "rb")
+	line = f.readline()    # throw away the first line
+#	for line in f :
+	while line :
+		# split the line by columns
+		line = f.readline()
+		line = line.rstrip()
+		lv = line.split('\t')
+
+		# Read in the gene types (# given in file)
+		if lv[0] == 'GENE TYPES' :
+			count = int(lv[2])
+			for i in range(0, count) :
+				line = f.readline()
+				line = line.rstrip()
+				lv = line.split('\t')
+				keepGenes.append(lv[1])
+			#end loop
+		# Read in the edge types (# given in file)
+		elif lv[0] == 'EDGE TYPES' :
+			count = int(lv[2])
+			for i in range(0, count) :
+				line = f.readline()
+				line = line.rstrip()
+				lv = line.split('\t')
+				keepEdges.append(lv[1])
+			#end loop
+		elif lv[0] == 'INDIRECT' :
+			count = int(lv[2])
+			for i in range(0, count) :
+				line = f.readline()
+				line = line.rstrip()
+				lv = line.split('\t')
+				keepEdges.append(lv[1])
+			#end loop
+		#end if
+	#end loop
+
+	return keepGenes, keepEdges, indirEdges
+
+#end func ###################
+
+
+
 
--- preProcessing.py
+++ preProcessing.py
@@ -2,3 +2,49 @@
 
 
 import preProcFuncs as pp
+
+
+####### ####### ####### ####### 
+# PARAMETERS
+
+ename = 'fakeNtwk00'
+epath = '../networks/'
+
+kfile = ename + '.keep.txt'
+
+
+#mfile = ename + '.metapaths.txt'
+#gfile = ename + '.genes.txt'
+#rfile = ename + '.meta.gene_pairs.txt'
+#pfile = ename + '.meta.path_names.txt'
+delim = '\t'
+
+####### ####### ####### ####### 
+
+
+# read in the keep file
+#   if not there, ask to have it created
+
+print "Reading in the network:", ename
+print "    reading the keep file", kfile
+keepGenes, keepEdges, indirEdges = pp.readKeepFile(
+    epath+kfile)
+
+#print keepGenes
+#print keepEdges
+#print indirEdges
+
+
+
+
+# read in the network to a matrix
+
+# read in the corrections file
+#   if not there, skip corrections
+#   also, alert user
+
+# ?
+# change indirect edges to direct
+# normalize weights
+
+# create matrices and network
