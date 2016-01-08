dc4099530b0b9a8a7904887737c474d202478a57
glinkowski <linkowski@hotmail.com>
2016-01-08 15:22:25 -0600

    creating more useful demo code
    mpFindPaths01 will read in a sample, get the count
    for each metapath within that sample, and rank the
    importance of each path. This is done by comparing
    to the path distribution of a random sampling of
    genes within the network.

--- mpFindFuncs.py
+++ mpFindFuncs.py
@@ -10,15 +10,49 @@
 #   
 #
 # Functions provided:
-#	readGeneFile(fname)
+#	readFileAsList(fname)
+#	readFileAsIndexDict(fname)
 # ---------------------------------------------------------
 
 import os.path
 import sys
+import numpy as np
 
 
 
-######## ######## ######## ########
+
+######## ######## ######## ######## 
+# Function: Read in a file as a line-by-line list of items
+# Input:
+#   fname, str - path + name of the the sample files
+# Returns:
+#	fItems, list of str - ordered list of items from file
+def readFileAsList(fname) :
+
+	# ERROR CHECK: verify file exists
+	if not os.path.isfile(fname) :
+		print ( "ERROR: Specified file doesn't exist:" +
+			" {}".format(fname) )
+		sys.exit()
+	#end if
+
+	# The list of items to return
+	fItems = list()
+
+	# Read in from the file
+	fn = open(fname, "rb")
+	for line in fn :
+		fItems.append( line.rstrip() )
+	#end loop
+	fn.close()
+
+	fItems.sort()
+	return fItems
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
 # Function: Read in the gene file. File is an ordered list
 #	of genes, where the row number (starting at zero)
 #	corresonds to the index in the matrix/list/etc where
@@ -29,7 +63,7 @@ import sys
 #	gDict, dict - 
 #		key, str: gene name as read from file
 #		value, int: index to corresponding array
-def readGeneFile(fname) :
+def readFileAsIndexDict(fname) :
 
 	# ERROR CHECK: verify file exists
 	if not os.path.isfile(fname) :
@@ -49,4 +83,51 @@ def readGeneFile(fname) :
 	#end loop
 
 	return gDict
-#end def ######## ######## ########
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Read in the dataset from a samplename
+#	Check for variants: ".txt", "_UP.txt", "_DN.txt"
+# Input:
+#   fname, str - path + name of the the sample files
+#   up, down: boolean -- only read the _UP or _DN file if true
+# Returns:
+#	sNodes, list of str - ordered list of names from file(s)
+def readSampleFiles(fname, up, down) :
+
+	# The list of items to return
+	sNodes = list()
+
+	# Flag indicates a file existed and was read
+	exists = False
+
+	# First look for the file as named (no _UP or _DN)
+	if os.path.isfile(fname + ".txt") :
+		temp = readFileAsList(fname + ".txt")
+		sNodes.extend(temp)
+		exists = True
+	#end if
+
+	# Look for the _DN file
+	if down and os.path.isfile(fname + "_DN.txt") :
+		temp = readFileAsList(fname + "_DN.txt")
+		sNodes.extend(temp)
+		exists = True
+	#end if
+
+	if up and os.path.isfile(fname + "_UP.txt") :
+		temp = readFileAsList(fname + "_UP.txt")
+		sNodes.extend(temp)
+		exists = True
+	#end if
+
+	# Alert user if nothing was read in
+	if not exists :
+		print "WARNING: no file found: {}".format(fname)
+
+	# Do NOT return duplicates
+	uNodes = np.unique(sNodes) # sorted list of unique items
+	return uNodes
+#end def ######## ######## ######## 
---
+++ mpFindPaths01.py
@@ -0,0 +1,73 @@
+# ---------------------------------------------------------
+# author: Greg Linkowski
+# project: Metapath Analysis
+#       for the KnowEnG big data center at UIUC
+#       funded by the NIH
+# 
+# Find all the metapaths in the network
+#
+# This is a basic demo. Load a sample file and rank all the
+#   paths found between the genes in that sample. Do this
+#   by comparing against random samples of the same size to
+#   see which paths are more pronounced in the sample.
+# Outline:
+#   
+# ---------------------------------------------------------
+
+import mpFindFuncs as ff
+import time
+
+
+
+####### ####### ####### ####### 
+# PARAMETERS
+
+# The network to use and directory path
+ename = 'fakeNtwk00_g2t3h10'
+epath = 'networks/'
+
+# The sample to test and path
+sname = 'Fake00_sample01'
+spath = 'samplesFake/'
+
+# Where to store the output
+oname = 'find01-' + ename + "-" + sname
+opath = 'outputFake/'
+
+# How many random samples to examine
+numRand = 13
+
+####### ####### ####### ####### 
+
+
+
+####### ####### ####### ####### 
+# BEGIN MAIN FUNCTION
+
+tstart = time.time()
+
+
+# open the sample file
+# get the list of paths available
+# for each path, store:
+#   this sample count (return left-out genes)
+#   rand: mean, std dev, z-score
+# output to file
+
+
+# 1) Read in the sample file
+
+print "Finding metapaths in sample: {}".format(sname)
+sampGenes = ff.readSampleFiles(spath + sname, True, True)
+print sampGenes
+
+# FOR REFERENCE: copied from mpFindPaths00
+## path to the metapath matrices
+#mpPath = epath + ename + "_MetaPaths/"
+## file containing the genes
+#gfile = "genes.txt"
+## Create mapping from gene name to row/col index
+#geneDict = ff.readGeneFile(mpPath + gfile)
+##print geneDict
+## output file
+#ofile = opath + oname + ".txt"
