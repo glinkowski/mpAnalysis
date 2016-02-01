412d6c39da1a5a4dd1bb0d6f106624a12817cb67
Shengliang <daishengliang@gmail.com>
2016-01-31 23:51:58 -0600

    add mpFindFuncs02 for Shengliang's functions

---
+++ mpFindFuncs02.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+
+# ---------------------------------------------------------
+# author: ShengliangDai
+# project: Metapath Analysis
+#		for the KnowEnG big data center at UIUC
+#		funded by the NIH
+# 
+# functions used to Find Paths in the processed network
+#
+# These functions were created to aid in the reading of a
+#	network, creation of random samples based on high/med/low, and sample matrices
+#	
+#   
+#
+# Functions provided:
+#	degree()
+#	nodeBining(thresholdHigh, thresholdLow, infile)
+#	sampleMatrix(sname)
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#			
+#	
+#	
+#			
+# ---------------------------------------------------------
+
+"""
+
