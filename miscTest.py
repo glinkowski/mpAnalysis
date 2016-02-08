b291c5ff6a74a091e7cfe6ee5a50c90c6b6b47b8
Greg L <linkowski@hotmail.com>
2016-02-08 13:34:10 -0600

    finished creating .keep file

--- miscTest.py
+++ miscTest.py
@@ -42,17 +42,35 @@ print 'Humans: {}'.format(hGenes)
 
 
 
-# Identify unique edge names in a network
-print ''
-fe = open('networks/all_v1.edge.txt', 'rb')
-edgeSet = set()
-for line in fe :
-	line = line.rstrip()
-	lv = line.split('\t')
+## Identify unique edge names in a network
+#print ''
+#fe = open('networks/all_v1.edge.txt', 'rb')
+#edgeSet = set()
+#for line in fe :
+#	line = line.rstrip()
+#	lv = line.split('\t')
+#
+#	if lv[3] not in edgeSet :
+#		edgeSet.add(lv[3])
+##end loop
+#edgeList = list(edgeSet)
+#edgeList.sort()
+#print 'Edges in all_v1: {}'.format(edgeList)
 
-	if lv[3] not in edgeSet :
-		edgeSet.add(lv[3])
-#end loop
-edgeList = list(edgeSet)
-edgeList.sort()
-print 'Edges in all_v1: {}'.format(edgeList)
+
+
+print ''
+ename = 'all_v1'
+epath = 'networks/'
+kfile = ename + '.keep.txt'
+# Read in the keep file
+#	if not there, ask to have it created
+print "\nReading in the network:", ename
+print "    reading the keep file", kfile
+geneHuman, keepGenes, loseGenes, keepEdges, indirEdges, thresh = pp.readKeepFile(epath+kfile)
+print 'Keep genes: {}'.format(keepGenes)
+print 'Ignore genes: {}'.format(loseGenes)
+print 'Keep edges: {}'.format(keepEdges)
+print 'Indirect: {}'.format(indirEdges)
+print 'Threshold = {}'.format(thresh)
+print 'Humans: {}'.format(hGenes)
