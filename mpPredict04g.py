34e8a5365a82eff5d817dd5224fcf5c8e7a0468e
Greg L <linkowski@hotmail.com>
2016-06-28 14:34:08 -0500

    saving Top 1 & Top 5 features selected

--- mpPredict04g.py
+++ mpPredict04g.py
@@ -55,14 +55,16 @@ useFeatPaths = True
 	# True/False: use the pathsim sum features
 useFeatNeighbor = True
 	# True/False: use the neighborhood features
-useGivenRange = np.linspace(0.00005, 0.002, num=19)
+useGivenRange = np.linspace(0.00005, 0.002, num=13)
 	# array of vals; 'None' means to auto-search for alphas
-useLabel = 'Lasso_Cluster_cP_fPN'
+useLabel = 'ClusVote_cLasP_fPN'
 	# string: label for the output files
+	# ie: ClusVote_c<Las/Enet/Log><P for Pos>_f<P for pathsim><N for neighborhood>
+#TODO: automate useLabel based on params
 
 
 # LASSO params
-lMaxIter = 3000
+lMaxIter = 2000
 lNorm = True
 lFitIcpt = True
 
@@ -191,6 +193,10 @@ for si in dSubDirs :
 	#	then predict on the rest
 	geneRanks = np.zeros( (len(geneDict), nClus), dtype=np.int32 )
 	geneScores = np.zeros( (len(geneDict), nClus), dtype=np.float32 )
+	pathT1Dict = dict()
+	pathT1Set = set()
+	pathT5Dict = dict()
+	pathT5Set = set()
 
 	col = -1
 	for cv in clusVals :
@@ -214,8 +220,8 @@ for si in dSubDirs :
 #		testIdx = np.reshape(testIdx, (len(testIdx),1))
 #		print(testIdx.shape)
 		testSet = features[testIdx,:]
-#		testLabel = featLabel[testIdx]
-#		testLabel = np.ravel(testLabel)
+		testLabel = featLabel[testIdx]
+		testLabel = np.ravel(testLabel)
 #		testNames = geneNames[testIdx]
 
 
@@ -283,6 +289,33 @@ for si in dSubDirs :
 		# 9) 
 #TODO: collect feature names & meaningful stats
 
+		# Extract indices corresponding to top 5 weighted features
+		featWeights = cfier.coef_
+		topPaths = np.ones( (5), dtype=np.int32 ) * (-1)
+		for num in range(5) :
+			if (len(np.nonzero(featWeights)[0]) <= num) :
+				break
+			featIdx = np.argmax(featWeights)
+			topPaths[num] = featIdx
+			featWeights[featIdx] = 0
+		#end if
+
+		# Increment count for the Top 1 path
+		if topPaths[0] in pathT1Set :
+			pathT1Dict[topPaths[0]] += 1
+		else :
+			pathT1Dict[topPaths[0]] = 1
+			pathT1Set.add(topPaths[0])
+		#end if
+
+		# Increment count for the Top 5 paths
+		for num in range(5) :
+			if topPaths[num] in pathT5Set :
+				pathT5Dict[topPaths[num]] += 1
+			else :
+				pathT5Dict[topPaths[num]] = 1
+				pathT5Set.add(topPaths[num])
+		#end loop
 
 
 #		break
@@ -295,6 +328,7 @@ for si in dSubDirs :
 
 	# 10) Output the ranked genes to file
 
+	# Get the AVERAGE gene rank
 	# First, vote across the clusters and sort by rank
 	# NOTE: any genes in the Known Set should have rank == 0
 	cfGenes = np.recarray( len(geneDict), dtype=[('geneIdx', 'i4'), ('rank', 'f4')])
@@ -303,7 +337,32 @@ for si in dSubDirs :
 	cfGenes.sort(order=['rank','geneIdx'])
 
 	# write the file
-	fname = 'ranked_genes-' + useLabel + '.txt'
+	fname = 'ranked_genes-' + useLabel + '_Avg.txt'
+	print("  Saving ranked genes to file {}".format(fname))
+	with open(si+fname, 'w') as fout :
+		firstRow = True
+		for row in range(len(cfGenes)) :
+			# Skip any genes whose rank == 0 (the Known Set)
+			if cfGenes['rank'][row] == 0 :
+				continue
+			#end if
+			if not firstRow :
+				fout.write('\n')
+			fout.write('{:3.3f}{}{}'.format(cfGenes['rank'][row],
+				textDelim, geneNames[cfGenes['geneIdx'][row]]))
+			firstRow = False
+	#end with
+
+	# Get the MEDIAN gene rank
+	# First, vote across the clusters and sort by rank
+	# NOTE: any genes in the Known Set should have rank == 0
+	cfGenes = np.recarray( len(geneDict), dtype=[('geneIdx', 'i4'), ('rank', 'f4')])
+	cfGenes['geneIdx'] = np.arange(len(geneDict))
+	cfGenes['rank'] = np.median(geneRanks, axis=1)
+	cfGenes.sort(order=['rank','geneIdx'])
+
+	# write the file
+	fname = 'ranked_genes-' + useLabel + '_Med.txt'
 	print("  Saving ranked genes to file {}".format(fname))
 	with open(si+fname, 'w') as fout :
 		firstRow = True
@@ -320,29 +379,48 @@ for si in dSubDirs :
 	#end with
 
 
-
 	# 11) Output the selected feature info to file
 #TODO: this!
 
+	# Sort the Top 1 paths
+	pathT1Sort = np.recarray( len(pathT1Dict), dtype=[('pathIdx', 'i4'), ('count', 'f4')])
+	pathIdxList = list(pathT1Dict.keys_)
+	row = -1
+	for item in pathIdxList :
+		row += 1
+		pathT1Sort['pathIdx'] = item
+		pathT1Sort['count'] = pathT1Dict[item]
+	#end if
+	pathT1Sort[::-1].sort(order=['count', 'pathIdx'])
 
-#	# Save the selected paths & scores/weights
-#	# 	feature coefficients are the metapath weights
-#	cfCoefs = np.nonzero(cfLassoStd.coef_)[0]
-#	cfPaths = np.recarray( len(cfCoefs), dtype=[('path', 'i4'), ('weight', 'f4')] )
-#	for row in range(len(cfCoefs)) :
-#		cfPaths[row] = (row, cfCoefs[row])
-#	cfPaths[::-1].sort(order=['weight', 'path'])	# sort by descending wieght
-#
-#	# write the file
-#	fname = 'ranked_paths-Lasso_2ClassStd.txt'
-#	print("Saving data for the Lasso standard approach ...")
-#	print("  Saving top paths to file {}".format(fname))
-#	with open(si+fname, 'w') as fout :
-#		fout.write('intercept:{}{}'.format(textDelim, cfLassoStd.intercept_))
-#		for row in range(len(cfPaths)) :
-#			fout.write('\n{}{}{}'.format(cfPaths['weight'][row],
-#				textDelim, pathNames[cfPaths['path'][row]]))
-#	#end with
+	# Save the Top 1 paths to file
+	fname = 'ranked_paths-' + useLabel + '_Top1.txt'
+	with open(si + fname, 'w') as fout :
+#		fout.write('intercept:{}{}'.format(textDelim, cfier.intercept_))
+		for row in range(len(pathT1Sort)) :
+			fout.write('\n{}{}{}'.format(pathT1Sort['count'][row],
+				textDelim, pathNames[pathT1Sort['pathIdx'][row]]))
+	#end with
+
+	# Sort the Top 5 paths
+	pathT5Sort = np.recarray( len(pathT5Dict), dtype=[('pathIdx', 'i4'), ('count', 'f4')])
+	pathIdxList = list(pathT5Dict.keys_)
+	row = -1
+	for item in pathIdxList :
+		row += 1
+		pathT5Sort['pathIdx'] = item
+		pathT5Sort['count'] = pathT5Dict[item]
+	#end if
+	pathT5Sort[::-1].sort(order=['count', 'pathIdx'])
+
+	# Save the Top 5 paths to file
+	fname = 'ranked_paths-' + useLabel + '_Top5.txt'
+	with open(si + fname, 'w') as fout :
+#		fout.write('intercept:{}{}'.format(textDelim, cfier.intercept_))
+		for row in range(len(pathT5Sort)) :
+			fout.write('\n{}{}{}'.format(pathT5Sort['count'][row],
+				textDelim, pathNames[pathT5Sort['pathIdx'][row]]))
+	#end with
 
 
 
@@ -353,9 +431,9 @@ for si in dSubDirs :
 		fout.write('Sampling Method for Neg examples\n')
 		fout.write('  as One-Class w/ clustering on the Unknown set\n')
 		fout.write('\n')
-		fout.write('using features\n')
-		fout.write('PathSim sum:{}{}'.format(textDelim, useFeatPaths))
-		fout.write('Neighborhood:{}{}'.format(textDelim, useFeatNeighbor))
+		fout.write('Features Used\n')
+		fout.write('PathSim sum:{}{}\n'.format(textDelim, useFeatPaths))
+		fout.write('Neighborhood:{}{}\n'.format(textDelim, useFeatNeighbor))
 		fout.write('\n')
 
 #TODO: collect some stats (ie: common alphas, l1 ratios, etc)
@@ -378,11 +456,11 @@ for si in dSubDirs :
 			fout.write('fit_intercept:{}{}\n'.format(textDelim, enFitIncept))
 		fout.write('\n')
 
-		fout.write('Similarity Metric:{}PathSim sum over set\n'.format(textDelim))
+#		fout.write('Similarity Metric:{}PathSim sum over set\n'.format(textDelim))
 		fout.write('Prediction Results\n')
-		fout.write('nonzero coefficients:{}{}\n'.format(textDelim, len(np.nonzero(cfLassoStd.coef_)[0])))
-		fout.write('Training score:{}{:3.3f}\n'.format(textDelim, cfLassoStd.score(trainSet, trainLabel)))
-		fout.write('Testing score:{}{:3.3f}\n'.format(textDelim, cfLassoStd.score(testSet, testLabel)))
+		fout.write('nonzero coefficients:{}{}\n'.format(textDelim, len(np.nonzero(cfier.coef_)[0])))
+		fout.write('Training score:{}{:3.3f}\n'.format(textDelim, cfier.score(trainSet, trainLabel)))
+		fout.write('Testing score:{}{:3.3f}\n'.format(textDelim, cfier.score(testSet, testLabel)))
 		fout.write('\n')
 	#end with
 
