456d00c89ac88a418c67cd031583feb908f528ba
Greg L <linkowski@hotmail.com>
2016-09-16 15:31:12 -0500

    built bulk of code, needs test run

--- mpChar01.py
+++ mpChar01.py
@@ -56,7 +56,8 @@ verboseOutput = True
 
 # Partition the samples and create z-score features
 # Input ----
-#	eDir, str: directory containing edge list
+#	eName, str: folder containing network files
+#	ePath, str: path to the network folder
 #	sDir, str: directory containing samples
 #	oDor, str: output directory
 #	printFlag, bool: whether to enable terminal output
@@ -66,15 +67,189 @@ verboseOutput = True
 #	directory in oDir containing folder for each sample
 #	(whole + 4 folds), containing gene partitions, and
 #	a matrix of z-score feature vectors for that sample
-def createFeatureZScore(eDir, sDir, oDir, printFlag) :
+def createFeatureZScore(eName, ePath, sDir, oRoot, printFlag) :
 
-	#1) Read in the samples
+	######## ######## ######## ######## 
+	# Parameters
+	percHide = 0.25
+#TODO: remove any superfluous params
+
+	#1) define an appropriate output directory
+	#	save parameters file
+	#2) Read in the samples
 	#	for each, create 4 folds
-	#2) define an appropriate output directory
-	#3) Save samples & folds to oDir
-	#4) Read in network info (gene names, etc)
-	#5) for each mp & set, create z-score feature
-	#6) save the feature matrix to each set
+	#	Save samples & folds to oDir
+	#3) Read in network info (gene names, etc)
+	#4) for each mp & set, create z-score feature
+	#5) save the feature matrix to each set
+
+
+
+	# 1) Name & create a folder to store output files
+	oSubDir = cl.nameOutputPath(oRoot, 'char01-batch')
+	if printFlag :
+		print("Files will be saved to {}".format(oSubDir))
+	oDir = oRoot + oSubDir
+
+	# Save experiment parameters to file
+	fOutput = list()
+	fOutput.append( ['date', 'network', 'ntwk path', '% hidden', 'samples'] )
+	fOutput.append( [time.strftime('%d/%m/%Y'), eName, ePath, percHide, sDir] )
+	fOutputName = 'parameters.txt'
+	cl.writeGenericLists(oDir, fOutputName, fOutput)
+
+
+	# 2a) Load the gene-index dict
+	if printFlag :
+		print("Creating the gene-index dictionary.")
+	geneDict = cl.getGeneDictionary(ePath, eName)
+
+	# 2b) Get the list of available paths
+	if printFlag :
+		print("Checking what paths are available ...")
+	pathDict = cl.getPathDictionary(ePath, eName)
+	pathList = cl.removeInvertedPaths(pathDict)
+
+
+#TODO: this should be a function
+	# Get expected matrix size
+	print("Finding the matrix dimensions ...")
+	fnMatrixZPad = 5
+	#TODO: pack this into a function
+	fname = (ePath+eName+"_MetaPaths/" +
+		"{}.gz".format(str(0).zfill(fnMatrixZPad)) )
+	mxSize = 0
+	with gzip.open(fname, 'rb') as fin :
+		for line in fin :
+			mxSize += 1
+	#end with
+
+
+
+	# 3) Read & partition samples, save to output dir
+	sNames = cl.getSampleNamesFromFolder(sDir)
+
+	oSampLists = list()
+	oSubDirList = list()
+
+	# Read samples & create cross-validation folds
+	sCount = 0
+	for s in sNames :
+		sCount += 1
+		if printFlag :
+			print("Collecting sample: {}, {}".format(sCount, s))
+
+		# Read in genes from the full sample
+		# Remove any genes not in the network
+		# Convert sample names to indices
+		gAll = cl.readSampleFiles(sDir + s, True, True)
+		gAllValid, gIgnored = cl.checkListAgainstDictKeys(
+			gAll, geneDict)
+		giAll = [geneDict[g] for g in gAllValid]
+
+		# Append the full sample to the lists
+		oSampLists.append(giAll)
+		oSubDir = '{}full-{}/'.format(oDir, s)
+		oSubDirList.append( oSubDir )
+
+		# Write the genes to file
+		saveListToText(oSubDir, 'known.txt', gAllValid)
+		saveListToText(oSubDir, 'hidden.txt', list() )
+		saveListToText(oSubDir, 'ignored.txt', gIgnored)
+
+		# Create the 4 folds
+		random.shuffle(giAll)
+		numFold = int(len(giAll) / 4)
+		for i in range(4) :
+			start = i * numFold
+			stop = (i * numFold) + numFold
+			gHidden = gAllValid[start:stop]
+			gHidden.sort()
+			gKnown = gAllValid[0:start]
+			gKnown.extend( gAllValid[stop:0])
+			gKnown.sort()
+
+			# Append this fold to the lists
+			giKnown = [geneDict[g] for g in gKnown]
+			oSampLists.append(giKnown)
+			oSubDirList.append( '{}part-{}-{02d}/'.format(sDir, s, i) )
+
+			# Write the genes to file
+			saveListToText(oSubDir, 'known.txt', gKnown)
+			saveListToText(oSubDir, 'hidden.txt', gHidden)
+			saveListToText(oSubDir, 'ignored.txt', list() )
+		#end loop range(4)
+	#end loop sNames
+
+
+
+	# 4) Create the z-score features
+
+
+	#	Build the feature vector matrices for each sample
+	gFeatures = np.zeros( (len(geneDict), len(pathList),
+		len(oSampLists)), dtype=np.float32 )
+
+	# populate dimension 2 from each path
+	dim2 = -1
+	for p in pathList :
+		dim2 += 1
+		
+		tpath = time.time()
+
+		# load the path count matrix
+		countMatrix = cl.getPathMatrix(pathDict[p], ePath, eName, mxSize)
+
+		# Calculate z-score over the columns
+		countAvg = np.mean(countMatrix, axis=0)
+		countAvg = countAvg.reshape( (1, len(countAvg)) )
+		countStD = np.std(countMatrix, axis=0)
+		countStD = countStD.reshape( (1, len(countStD)) )
+		countStD = np.add(countStD, 0.0001)
+
+		# convert to z-scores on a row-by-row basis
+		simMatrix = np.subtract(countMatrix, countAvg)
+		simMatrix = np.divide(countMatrix, countStD)
+
+		# zero out the diagonal (remove gene self-similarity)
+		np.fill_diagonal(simMatrix, 0)
+
+		# populate dimension 3 from each sample
+		dim3 = -1
+		for giList in oSampLists :
+			dim3 += 1
+
+			# calculate feature for this sample variant
+			simSet = np.sum( simMatrix[:,giList], axis=1 )
+			gFeatures[:,dim2,dim3] = simSet[:]
+		#end loop
+
+		if printFlag and not (dim2 % 25) :
+			print("  Examined {} of {} paths...".format(dim2, len(pathList)))
+			print("    --time per path: {:.3} (s)".format(time.time()-tpath))
+#TODO: convert into a "time remaining / ETA"
+	#end loop
+	if printFlag :
+		print("Finished examining matrix similarity matrices.")
+
+
+#TODO: clean this
+	# 5) ...
+	Save the raw feature vectors to the sample folders
+	i = -1
+	twrite = time.time()
+	for sDir in oSubDirList :
+		i += 1
+
+		mp.saveMatrixNumpy(gFeatures[:,:,i], 'features_ZScoreSim',
+			sDir, False)
+	#end loop
+	print("Finished writing Z-Score Similarity feature vector files.")
+	print("    --elapsed time: {:.3} (s)".format(time.time()-twrite))
+
+
+
+
 
 
 #end def ######## ######## ######## 
@@ -91,8 +266,8 @@ print("\nCreating z-score features for samples in {}".format(sPath))
 
 # Main Function call
 print("Calling function createFeatureZScore() ...")
-eDir = cl.concatenatePaths(ePath, eName)
-createFeatureZScore(eDir, sPath, oRoot, verboseOutput)
+#eDir = cl.concatenatePaths(ePath, eName)
+createFeatureZScore(eName, ePath, sPath, oRoot, verboseOutput)
 
 print("--elapsed time: {:.3} (s)".format(time.time()-tstart))
 
--- mpCharLib.py
+++ mpCharLib.py
@@ -13,13 +13,13 @@
 # ---------------------------------------------------------
 
 import os.path
-from os import listdir
-import sys
-import numpy as np
-import random
-import gzip
-from sklearn import cluster as skc
-import warnings
+#from os import listdir
+#import sys
+#import numpy as np
+#import random
+#import gzip
+#from sklearn import cluster as skc
+#import warnings
 
 
 
@@ -45,8 +45,9 @@ def verifyDirectory(path, create, quiet) :
 	exists = True
 	if not os.path.isdir(path) :
 		if create :
-			print("Creating path: {}".format(path))
 			os.makedirs(path)
+			if verbose :
+				print("Creating path: {}".format(path))
 		elif quiet :
 			exists = False
 		else :
@@ -54,6 +55,7 @@ def verifyDirectory(path, create, quiet) :
 				" {}".format(path) )
 			sys.exit()
 	#end if
+
 	return exists
 #end def ######## ######## ######## 
 ######## ######## ######## ########
@@ -107,4 +109,422 @@ def concatenatePaths(root, subDir) :
 	path = root + subDir
 
 	return path
-#end def ####### ####### ####### 
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Choose an unused name for the output path
+# Input ----
+#   path, str: path to where output should be saved
+#   dirPre, str: prefix of the folder name to return
+# Returns ----
+#   dirFull, str: name of output file (without path)
+def nameOutputPath(path, dirPre) :
+
+	# ERROR CHECK: verify directory exists
+	verifyDirectory(path, False, False)
+
+	zpad = fnOutputZPad
+
+	# Set of all sub-folders in the path
+	dirSet = [name for name in os.listdir(path)
+		if os.path.isdir(path + name)]
+
+	# increment folder name until an unused one is found
+	num = int(0)
+	dirFull = dirPre + "-{}".format(str(num).zfill(zpad))
+	while dirFull in dirSet :
+		num += 1
+		dirFull = dirPre + "-{}".format(str(num).zfill(zpad))
+	#end loop
+	dirFull = dirFull + '/'
+
+	return dirFull
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Write a text file where the columns are given as lists
+# Input ----
+#	path, str: directory to write output file
+#	fname, str: name of the file to write
+#	columnList, list of str lists:
+#		each entry in columnList represents a column
+#		where each entry is a string to write to the file
+# Returns ----
+#	nothing
+# Creates ----
+#	ranked_paths.txt: original version of the output file
+def writeGenericLists(path, fname, columnList) :
+
+	verifyDirectory(path, True, False)
+
+# ASSUME: the contained lists are of equal length
+
+	fout = open(path+fname, 'w')
+
+	for i in range(len(columnList[0])) :
+		fout.write("{}".format(columnList[0][i]))
+
+		for j in range(1, len(columnList)) :
+			fout.write("{}{}".format(textDelim, columnList[j][i]))
+		#end loop
+
+		fout.write("\n")
+	#end if
+
+	fout.close()
+
+	return
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Read in the gene file. File is an ordered list
+#   of genes, where the row number (starting at zero)
+#   corresonds to the index in the matrix/list/etc where
+#   the gene can be found.
+# Input ----
+#   fname, str: path & name to keep file
+# Returns ----
+#   iDict, dict: 
+#       key, str: gene name as read from file
+#       value, int: index to corresponding array
+def readFileAsIndexDict(fname) :
+
+	verifyFile('', fname, False)
+
+	# Build the dictionary from the text file
+	iDict = dict()
+	gf = open(fname, "r")
+	index = 0
+	for line in gf :
+		gene = line.rstrip()    # remove "\n"
+		iDict[gene] = int(index)
+		index += 1
+	#end loop
+
+	return iDict
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Read in the genes.txt file containing the 
+#   gene-name headers to the metapath matrices
+# Pre-reqs: readFileAsIndexDict(fname)
+# Input ----
+#   path, str: path to the network files
+#   name, str: name of the network to use
+# Returns ----
+#   gDict, dict
+#       key, str: name of gene
+#       value, int: row/col index for that gene
+def getGeneDictionary(path, name) :
+
+	fname = path + name + "_MetaPaths/genes.txt"
+
+	# The item to return
+	gDict = readFileAsIndexDict(fname)
+
+	return gDict
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Read in the key.txt file regarding the 
+#   metapath matrices
+# Input ----
+#   path, str: path to the network files
+#   name, str: name of the network to use
+# Returns ----
+#   keyDict, dict
+#       key, str: name of metapath
+#       value, tuple: int is matrix/file ID number
+#           bool where True means use matrix transpose
+def getPathDictionary(path, name) :
+
+	fname = path + name + "_MetaPaths/key.txt"
+
+	# ERROR CHECK: verify file exists
+	if not os.path.isfile(fname) :
+		print ( "ERROR: Specified file doesn't exist:" +
+			" {}".format(fname) )
+		sys.exit()
+	#end if
+
+	# The item to return
+	keyDict = dict()
+
+	# Read in the file
+	fk = open(fname, "r")
+	firstline = True
+	for line in fk :
+
+		# skip the first line
+		if firstline :
+			firstline = False
+			continue
+		#end if
+
+		# separate the values
+		line = line.rstrip()
+#		print(line)
+		lk = line.split('\t')
+		lv = lk[0].split(',')
+
+		transpose = False
+		if lv[1] == "t" :
+			transpose = True
+		#end if
+
+		# add to the dict
+		keyDict[lk[1]] = [int(lv[0]), transpose]
+	#end loop
+	fk.close()
+
+	return keyDict
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Load the matrix containing the number of paths
+#   of this type which join the nodes in the network
+# Input ----
+#   mpTuple [int, bool]: indicates which matrix file to use
+#   path, str: path to the network files
+#   name, str: name of the network to use
+# Returns ----
+#   matrix, int array: num paths between node pairs
+def getPathMatrix(mpTuple, path, name, sizeOf) :
+
+#TODO: change name(s) from "get..." to "load..."
+
+	zpad = fnMatrixZPad
+#   fname = (path + name + "_MetaPaths/" +
+#       "{}.npy".format(str(mpTuple[0]).zfill(zpad)) )
+
+	prename = (path + name + "_MetaPaths/" +
+		"{}".format(str(mpTuple[0]).zfill(zpad)) )
+	if os.path.isfile(prename + '.gz') :
+		fname = (path + name + "_MetaPaths/" +
+		"{}.gz".format(str(mpTuple[0]).zfill(zpad)) )
+	elif os.path.isfile(prename + '.txt') :
+		fname = (path + name + "_MetaPaths/" +
+		"{}.txt".format(str(mpTuple[0]).zfill(zpad)) )
+	else :
+		# ERROR CHECK: verify file exists
+		print ( "ERROR: Specified file doesn't exist:" +
+			" {}".format(fname) )
+		sys.exit()
+	#end if
+
+	# Load the matrix
+#   matrix = np.load(fname)
+#	matrix = np.loadtxt(fname)
+
+	# Declare the matrix
+	if speedVsMemory :
+		matrix = np.zeros([sizeOf, sizeOf])
+	else :
+		matrix = np.zeros([sizeOf, sizeOf], dtype=matrixDT)
+	#end if
+
+	# Read in the file, placing values into matrix
+	row = 0
+	with gzip.open(fname, 'rb') as fin :
+		for line in fin :
+			line = line.rstrip()
+			ml = line.split()
+			matrix[row,:] = ml[:]
+			row += 1
+	#end with
+
+	# Convert to transpose if flag==True
+	if mpTuple[1] :
+		return np.transpose(matrix)
+	else :
+		return matrix
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Find the number of paths of this type joining
+#   the nodes in the sample
+# Input ----
+#   mpDict, {str: [int, bool]} dict:
+#       key, str - name of the metapath
+#       value, [int, bool] - which matrix file to use, and 
+#           whether to use the transpose (inverse path)
+# Returns ----
+#   mpList, str list: ordered names of paths available,
+#       less paths that are mirror-images of another
+def removeInvertedPaths(mpDict) :
+
+	# The item to return
+	mpList = list()
+
+	# Check the keys in the dict
+	for key in mpDict.keys() :
+		# If the boolean is True, then the path is an
+		#   inverse of another; only append if false
+		if mpDict[key][1]==False :
+			mpList.append(key)
+	#end loop
+
+	mpList.sort()
+	return mpList
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: Create a list of samples contained in folder
+# Input ----
+#	sPath, str: path where samples stored
+# Returns ----
+#	sNames, str list: sorted list of sample names
+def getSampleNamesFromFolder(path) :
+
+	verifyDirectory(path, False, False)
+
+	# Get list of all text files in folder
+	fNames = [f for f in listdir(path) if f.endswith('.txt')]
+	if verbose :
+		print (fNames)
+
+	# Identify & create list of sample names in folder
+	sNames = list()
+	for item in fNames :
+		# Strip the extension and any "_UP" or "_DN"
+		newItem = item[:-4]
+		if newItem.endswith('_UP') or newItem.endswith('_DN') :
+			newItem = newItem[:-3]
+		#end if
+		sNames.append(newItem)
+	#end loop
+
+	sNames = np.unique(sNames)	# also sorts list
+	return sNames
+#end def ######## ######## ######## 
+
+
+
+
+######## ######## ######## ######## 
+# Function: Read in the dataset from a samplename
+#   Check for variants: ".txt", "_UP.txt", "_DN.txt"
+# Pre-reqs: readFileAsList(fname)
+# Input ----
+#   fname, str: path + name of the the sample files
+#   up, down: boolean -- only read the _UP or _DN file if true
+# Returns ----
+#   sNodes, str list: ordered list of names from file(s)
+def readSampleFiles(sfile, up, down) :
+
+	# The list of items to return
+	sNodes = list()
+
+	# Flag indicates a file existed and was read
+	exists = False
+
+	# First look for the file as named (no _UP or _DN)
+	if os.path.isfile(sfile + ".txt") :
+		temp = readFileAsList(sfile + ".txt")
+		sNodes.extend(temp)
+		exists = True
+	#end if
+
+	# Look for the _DN file
+	if down and os.path.isfile(sfile + "_DN.txt") :
+		temp = readFileAsList(sfile + "_DN.txt")
+		sNodes.extend(temp)
+		exists = True
+	#end if
+
+	if up and os.path.isfile(sfile + "_UP.txt") :
+		temp = readFileAsList(sfile + "_UP.txt")
+		sNodes.extend(temp)
+		exists = True
+	#end if
+
+	# Alert user if nothing was read in
+	if not exists :
+		print("WARNING: no file found: {}".format(sfile))
+
+	# Do NOT return duplicates
+	uNodes = np.unique(sNodes) # sorted list of unique items
+	return uNodes
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Given a list of items, remove any items not in specified dict
+# Input ----
+#	theList, xxx list: list of items that may not be in theDict
+#	theDict, xxx dict: dictionary against which to check (the keys)
+# Returns ----
+#   inList & outList, xxx list: which items from the
+#       given list are found in dict & which aren't
+def checkListAgainstDictKeys(theList, theDict) :
+
+	# The items to return
+	inList = list()
+	outList = list()
+
+	# extract the keys as a set
+	keySet = set(theDict.keys())
+
+	# Sift through the sample
+	for item in theList :
+		if item in keySet :
+			inList.append(item)
+		else :
+			outList.append(item)
+		#end if
+	#end loop
+
+	inList.sort()
+	outList.sort()
+	return inList, outList
+#end def ######## ######## ######## 
+
+
+
+######## ######## ######## ######## 
+# Function: save a list to a text file
+# Input:
+#   path, str: path to save the file
+#   name, str: name of file to save
+#   theList, list of str - list of items to save
+#       ASSUMPTION: list is already properly ordered
+# Returns: nothing
+# Creates: file containing ordered list of items
+def saveListToText(path, name, theList) :
+
+	# If folder doesn't exist, create it
+	if not os.path.exists(path) :
+		os.makedirs(path)
+	#end if
+
+	theFile = open(path + name, 'w')
+	firstline = True
+	for item in theList :
+		if firstline :
+			firstline = False
+		else :
+			theFile.write("\n")
+		#end if
+		theFile.write("{}".format(item))
+	#end if
+	theFile.close()
+
+	return
+#end def ######## ######## ######## 
